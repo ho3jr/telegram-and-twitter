02209f08e49055d9e91d9e1fd8d549fe4a98b964
ho3jr <ho3jr1@gmail.com>
2024-08-16 15:44:06 +0330

    v1.1 | msg added

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton 
 import pyromod
 
-api_id = 18790467
-api_hash = "68bf0527a74571a8dbb9cfffe70ef964"
-token = "5671269812:AAGEN3LqRU4oQ8Zg1-hrSjQUDpyBJwQhq20"
+api_id = 111111
+api_hash = " "
+token = " "
 
 app = Client(
     "my_bot",
@@ -19,13 +19,16 @@ yes_or_no = InlineKeyboardMarkup(
     ]
 )
 
+msg= ""
 
 @app.on_message(filters.channel)
 async def recive_msg_channel(c: Client, m: Message):
     if m.chat.id == -1002193502389:
         if m.media:
+            msg = str(m.caption)
             await app.copy_message(int("5406865643"),int(m.chat.id), m.id , caption=m.caption+"\n\n----------------\n"+"میخوای توییتش کنم؟",reply_markup=yes_or_no)
         else:
+            msg = str(m.text)
             await app.copy_message(int("5406865643"),int(m.chat.id), m.id )
             await app.send_message(int("5406865643"),"میخوای توییتش کنم؟", reply_markup= yes_or_no)
 
@@ -35,9 +38,11 @@ async def recive_query(Client, call1):
 
     if data == "yes":
         pass
+        msg = ""
 
     elif data =="no":
         pass
+        msg =""
     
 
 app.run()
